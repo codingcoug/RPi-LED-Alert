2b777d642667a47dbac44097e891e2b067bb4732
Seth Whiting <swhitin7@byu.edu>
2019-08-13 11:23:08 -0600

    created python alert script

---
+++ alert.py
@@ -0,0 +1,64 @@
+import RPi.GPIO as GPIO
+import time
+from neopixel import *
+import argparse
+
+# LED strip configuration:
+LED_COUNT      = 71      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+GPIO.add_event_detect(17, GPIO.BOTH)
+#def my_callback():
+#    GPIO.output(25, GPIO.input(17))
+#GPIO.add_event_callback(17, my_callback)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+if __name__ == '__main__':
+
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+
+    try:
+        while True:
+            if GPIO.input(17)
+                theaterChaseRainbow(strip, 0)
+            else
+                colorWipe(strip, Color(0, 0, 0), 0)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        colorWipe(strip, Color(0, 0, 0), 0)

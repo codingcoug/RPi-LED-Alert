670459a0654a7de30e3f091cd47a09f406465917
Seth Whiting <swhitin7@byu.edu>
2019-08-14 09:26:00 -0600

    added wheel def

--- alert.py
+++ alert.py
@@ -26,6 +26,18 @@ GPIO.add_event_detect(17, GPIO.BOTH)
 #    GPIO.output(25, GPIO.input(17))
 #GPIO.add_event_callback(17, my_callback)
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
 def theaterChaseRainbow(strip, wait_ms=50):
     """Rainbow movie theater light style chaser animation."""
     for j in range(256):

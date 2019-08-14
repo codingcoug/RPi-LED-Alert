877f1dd9c92509f24d9b7ba0dae95bc171e51b03
Seth Whiting <swhitin7@byu.edu>
2019-08-14 11:58:42 -0600

    if statement manip

--- alert.py
+++ alert.py
@@ -3,28 +3,10 @@ import time
 from neopixel import *
 import argparse
 
-# LED strip configuration:
-LED_COUNT      = 71      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
 strip.begin()
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
-GPIO.add_event_detect(17, GPIO.BOTH)
-#def my_callback():
-#    GPIO.output(25, GPIO.input(17))
-#GPIO.add_event_callback(17, my_callback)
 
 def wheel(pos):
     """Generate rainbow colors across 0-255 positions."""
@@ -56,6 +38,27 @@ def colorWipe(strip, color, wait_ms=50):
         strip.show()
         time.sleep(wait_ms/1000.0)
 
+# LED strip configuration:
+LED_COUNT      = 71      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+GPIO.add_event_detect(17, GPIO.BOTH)
+#def my_callback():
+#    GPIO.output(25, GPIO.input(17))
+#GPIO.add_event_callback(17, my_callback)
+
+
 if __name__ == '__main__':
 
     # Process arguments
@@ -73,7 +76,7 @@ if __name__ == '__main__':
     try:
         while True:
             print("output of gpio 17:",GPIO.input(17))
-            if GPIO.wait_for_edge(17, GPIO.RISING)
+            #if GPIO.wait_for_edge(17, GPIO.RISING)
             theaterChaseRainbow(strip, 20)
             #else
             #    colorWipe(strip, Color(0, 0, 0), 0)

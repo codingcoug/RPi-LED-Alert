767360493d5b71dd89f7f7c79cf3b474b8e4798b
Seth Whiting <swhitin7@byu.edu>
2019-08-14 09:58:16 -0600

    if statement manip

--- alert.py
+++ alert.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     try:
         while True:
             print("output of gpio 17:",GPIO.input(17))
-            #if
+            if GPIO.wait_for_edge(17, GPIO.RISING)
             theaterChaseRainbow(strip, 20)
             #else
             #    colorWipe(strip, Color(0, 0, 0), 0)
